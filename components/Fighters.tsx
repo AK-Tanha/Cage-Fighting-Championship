e9cb4b9666892013faf91ce54bca3923b2482969
AK Tanha <aktanha75@gmail.com>
2026-01-25 16:46:57 +0600

    style: reorder React named imports.

--- components/Fighters.tsx
+++ components/Fighters.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { getAllFighters } from '../API/fighters'; // your API file
 import { Fighter } from '../types';
 
